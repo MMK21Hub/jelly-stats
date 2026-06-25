bac7200ce1c51516cd3b688b45ed1dc8e3db7848
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-06-25 12:05:11 +0100

    testing in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use anyhow::{Context, Ok, Result};
-use jelly_stats::jelly::JellyClient;
+use jelly_stats::jelly::{ConversationListOptions, JellyClient};
 
 fn main() -> Result<()> {
     dotenv::dotenv().ok();
 
     let client = JellyClient::new(
-        std::env::var("JELLY_API_URL").unwrap_or("https://app.letsjellyaaa.com/api".to_string()),
+        std::env::var("JELLY_API_URL").unwrap_or("https://app.letsjelly.com/api".to_string()),
         std::env::var("JELLY_API_KEY").context("JELLY_API_KEY must be set")?,
     )?;
+
+    let convos = client.count_conversations(&ConversationListOptions {
+        // label_id: Some("".to_string()),
+        mailbox_id: Some("stardance".to_string()),
+        ..Default::default()
+    });
+
+    println!("Conversations: {:#?}", convos);
 
     Ok(())
 }
